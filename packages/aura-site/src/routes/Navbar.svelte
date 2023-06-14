307a9c327829f069fb1efb5663d68f533e317d43
theonlytails <theonlytails@theonlytails.com>
2023-06-14 21:19:36 +0300

    added a github link in the navbar

--- packages/aura-site/src/routes/Navbar.svelte
+++ packages/aura-site/src/routes/Navbar.svelte
@@ -1,40 +1,54 @@
 <script context="module" lang="ts">
-  export type NavbarEntry = {
-    name: string;
-    path: string;
-    icon: any;
-  };
+	export type NavbarEntry = {
+		name: string
+		path: string
+		icon: any
+	}
 </script>
 
 <script lang="ts">
-  import Book from "~icons/material-symbols/developer-guide";
-  import Toy from "~icons/material-symbols/home";
+	import { externalLink } from "$lib/utils"
 
-  const entries = [
-    {
-      name: "Docs",
-      path: "/docs",
-      icon: Book,
-    },
-    // {
-    //   name: "Playground",
-    //   path: "/playground",
-    //   icon: Toy,
-    // },
-  ] satisfies NavbarEntry[];
+	import Book from "~icons/material-symbols/developer-guide"
+	import Toy from "~icons/material-symbols/home"
+	import Github from "~icons/mdi/github"
+
+	const entries = [
+		{
+			name: "Docs",
+			path: "/docs",
+			icon: Book,
+		},
+		// {
+		//   name: "Playground",
+		//   path: "/playground",
+		//   icon: Toy,
+		// },
+	] satisfies NavbarEntry[]
 </script>
 
 <nav class="navbar sticky top-0 z-50 w-full h-14 bg-base-200 border-b border-b-divider">
-  <a href="/" class="btn hover:bg-transparent hover:border-transparent text-2xl normal-case">Aura</a>
+	<a href="/" class="btn hover:bg-transparent hover:border-transparent text-2xl normal-case">Aura</a
+	>
 
-  <ul class="menu menu-horizontal">
-    {#each entries as { name, path, icon }}
-      <li>
-        <a href={path}>
-          <svelte:component this={icon} />
-          {name}
-        </a>
-      </li>
-    {/each}
-  </ul>
+	<ul class="flex-1 menu menu-horizontal">
+		{#each entries as { name, path, icon }}
+			<li>
+				<a href={path}>
+					<svelte:component this={icon} />
+					{name}
+				</a>
+			</li>
+		{/each}
+	</ul>
+
+	<a
+		href="https://github.com/theonlytails/aura-lang"
+		class="btn btn-ghost text-lg btn-circle flex-none"
+    title="GitHub Repo"
+    aria-label="GitHub Repo"
+		{...externalLink}
+	>
+		<Github />
+	</a>
 </nav>
